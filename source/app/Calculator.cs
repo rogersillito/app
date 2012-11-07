60406ee7b046f9b036b027704478fabe0e96230f
chillinscott <marcus.scott.burleigh@gmail.com>
2012-11-07 15:45:01 -0600

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -17,6 +17,8 @@ namespace app
       if(i < 0 || i1 < 0)
           throw new ArgumentException("Negatives aren't allowed");
         connection.Open();
+        var command = connection.CreateCommand();
+        command.ExecuteNonQuery();
       return i + i1;
     }
   }
