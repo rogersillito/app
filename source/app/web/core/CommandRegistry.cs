a5e8102a34bfee28f1b9129b9745a735a45391b0
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-11-07 16:19:35 -0700

    Updated

--- source/app/web/core/CommandRegistry.cs
+++ source/app/web/core/CommandRegistry.cs
@@ -2,6 +2,7 @@
 {
   public class CommandRegistry : IFindCommands
   {
+
     public IProcessOneRequest get_the_command_that_can_run(IContainRequestDetails request)
     {
       throw new System.NotImplementedException();
