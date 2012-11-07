27bcefe2bdd6d55d23239885e9555b7abf7fddf5
chillinscott <marcus.scott.burleigh@gmail.com>
2012-11-07 17:12:22 -0600

    Pushing new changes

--- source/app/web/core/FrontController.cs
+++ source/app/web/core/FrontController.cs
@@ -2,9 +2,16 @@
 {
   public class FrontController : IProcessRequests
   {
-    public void process(IContainRequestDetails request)
-    {
-      throw new System.NotImplementedException();
-    }
+      readonly IFindCommands command_registry;
+
+      public FrontController(IFindCommands command_registry)
+      {
+          this.command_registry = command_registry;
+      }
+
+      public void process(IContainRequestDetails request)
+      {
+          command_registry.get_the_command_that_can_run(request).run(request);
+      }
   }
 }
