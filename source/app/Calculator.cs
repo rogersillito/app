eb69cca646b67eb34c4609d651b1ac15a4319e9d
chillinscott <marcus.scott.burleigh@gmail.com>
2012-11-07 15:30:54 -0600

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,9 +1,13 @@
-﻿namespace app
+﻿using System;
+
+namespace app
 {
   public class Calculator
   {
     public int add(int i, int i1)
     {
+      if(i < 0 || i1 < 0)
+          throw new ArgumentException("Negatives aren't allowed");
       return i + i1;
     }
   }
