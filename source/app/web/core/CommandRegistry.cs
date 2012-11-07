f2d61d283905d44b44066277b5b1cd922490ee0f
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-11-07 16:26:41 -0700

    Updated

--- source/app/web/core/CommandRegistry.cs
+++ source/app/web/core/CommandRegistry.cs
@@ -1,37 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace app.web.core
 {
   public class CommandRegistry : IFindCommands
   {
-      IEnumerable<IProcessOneRequest> commands;
+    IEnumerable<IProcessOneRequest> commands;
 
-      public CommandRegistry(IEnumerable<IProcessOneRequest> commands)
-      {
-          this.commands = commands;
-      }
-
-      public IProcessOneRequest get_the_command_that_can_run(IContainRequestDetails request)
+    public CommandRegistry(IEnumerable<IProcessOneRequest> commands)
     {
-          foreach (var command in commands)
-          {
-              if (command.can_run(request))
-                  return command;
-          }
-          return new EmptyCommand();
+      this.commands = commands;
+    }
+
+    public IProcessOneRequest get_the_command_that_can_run(IContainRequestDetails request)
+    {
+      return commands.First(x => x.can_run(request));
     }
   }
 
-    public class EmptyCommand :IProcessOneRequest
-    {
-        public void run(IContainRequestDetails request)
-        {
-            
-        }
-
-        public bool can_run(IContainRequestDetails request)
-        {
-            throw new System.NotImplementedException();
-        }
-    }
 }
