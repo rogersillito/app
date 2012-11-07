5ea48ac78a7d6e594ba95d015d51692213df6c0c
chillinscott <marcus.scott.burleigh@gmail.com>
2012-11-07 17:33:24 -0600

    Pushing new changes

--- source/app/web/core/CommandRegistry.cs
+++ source/app/web/core/CommandRegistry.cs
@@ -7,16 +7,21 @@ namespace app.web.core
   public class CommandRegistry : IFindCommands
   {
     IEnumerable<IProcessOneRequest> commands;
+      readonly MissingCommandFactory_Behaviour function_tohandle_command_not_found;
 
-    public CommandRegistry(IEnumerable<IProcessOneRequest> commands)
+      public CommandRegistry(IEnumerable<IProcessOneRequest> commands, MissingCommandFactory_Behaviour function_tohandle_command_not_found)
     {
-      this.commands = commands;
+        this.commands = commands;
+        this.function_tohandle_command_not_found = function_tohandle_command_not_found;
     }
 
-    public IProcessOneRequest get_the_command_that_can_run(IContainRequestDetails request)
-    {
-      return commands.First(x => x.can_run(request));
-    }
+      public IProcessOneRequest get_the_command_that_can_run(IContainRequestDetails request)
+      {
+          var command = commands.First(x => x.can_run(request));
+          if (command == null)
+              return function_tohandle_command_not_found();
+          return command;
+      }
   }
 
 }
