7be92b60104ea6a84ef14be4774fbbd5e90f695f
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-11-07 14:22:20 -0700

    Updated

--- source/app.specs/CalculatorSpecs.cs
+++ source/app.specs/CalculatorSpecs.cs
@@ -6,6 +6,7 @@ namespace app.specs
   {
     public class when_adding_two_numbers
     {
+      //assert
       It should_return_the_sum = () =>
         Calculator.add(2, 3).ShouldEqual(5);
         
