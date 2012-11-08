7b0610b2b587d37332898982cee89f1784415df3
Roger Sillito <rogersillito@gmail.com>
2012-11-08 15:48:57 -0700

    Pushing new changes

--- source/app/web/core/aspnet/WebFormDisplayEngine.cs
+++ source/app/web/core/aspnet/WebFormDisplayEngine.cs
@@ -1,17 +1,22 @@
-﻿namespace app.web.core.aspnet
+﻿using System.Web;
+
+namespace app.web.core.aspnet
 {
-  public class WebFormDisplayEngine : IDisplayInformation
-  {
-    ICreateViewsForReports view_factory;
-
-    public WebFormDisplayEngine(ICreateViewsForReports view_factory)
+    public class WebFormDisplayEngine : IDisplayInformation
     {
-      this.view_factory = view_factory;
-    }
+        ICreateViewsForReports view_factory;
+        readonly HttpContext http_context;
 
-    public void display<ReportModel>(ReportModel model)
-    {
-      view_factory.create_view_that_can_display(model);
+        public WebFormDisplayEngine(ICreateViewsForReports view_factory, HttpContext http_context)
+        {
+            this.view_factory = view_factory;
+            this.http_context = http_context;
+        }
+
+        public void display<ReportModel>(ReportModel model)
+        {
+            IHttpHandler view = view_factory.create_view_that_can_display(model);
+            view.ProcessRequest(http_context);
+        }
     }
-  }
 }
